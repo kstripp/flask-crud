62bcb7251c7cfee0e76d51883bc87946a6a91deb
K. Scott Tripp <kscott.tripp@gmail.com>
2016-05-08 00:16:07 -0700

    Starting test cases
    Attempting to programatically set up a database during each test run

--- test.py
+++ test.py
@@ -5,22 +5,33 @@ import os
 import unittest
 import tempfile
 from app import app, db
-from flask.ext.migrate import Migrate 
+from flask.ext.migrate import Migrate #, init, migrate, upgrade
+import flask.ext.migrate
 
 class CrudTestCase(unittest.TestCase):
 
     def setUp(self):
         self.db_fd, self.fname = tempfile.mkstemp()
+        self.dirname = tempfile.mkdtemp()
         dbase_uri = 'sqlite://' + os.path.join(self.fname)
         app.config["SQLALCHEMY_DATABASE_URI"] = dbase_uri
         #app.config['TESTING'] = True
         migrate = Migrate(app, db)
-        
+        self.app_context = app.app_context()
+        self.app_context.push()
         self.app = app.test_client()
+        
+        with app.app_context():
+            flask.ext.migrate.init(directory=self.dirname)
+            flask.ext.migrate.migrate()
+            flask.ext.migrate.upgrade()
+        
 
     def tearDown(self):
+        self.app_context.pop()
         os.close(self.db_fd)
         os.unlink(self.fname)
+        os.unlink(self.dirname)
     
     def test_empty_db(self):
         rv = self.app.get('/')
